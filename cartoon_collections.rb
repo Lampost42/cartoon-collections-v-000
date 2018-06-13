641ed6875264bef04151417f6c9a4781ce8b0c27
Trevor Russell <truss.42@gmail.com>
2018-06-13 20:19:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.detect {|word| cheese_types.include? {word}}
+  array.detect {|word| cheese_types.include?(word)}
 end
